c247dae1bc3e1720ef284f0e558a2e461c003d71
ShivamKapadia <88721707+ShivamKapadia@users.noreply.github.com>
2021-11-23 11:31:18 +0530

    Update 35 to 70.js

--- 35 to 70.js
+++ 35 to 70.js
@@ -1,3 +1,41 @@
-function 35to70.js();
-{
+function svk();
+{ 
+     document.write ("35-Bromine" + "<br>" );
+     document.write ("36-Krypton" + "<br>");
+     document.write ("37-Rubidium"+ "<br>");
+     document.write ("38-Strontium"+ "<br>");
+     document.write ("39-Yttrium"+ "<br>");
+     document.write ("40-Zirconium"+ "<br>");
+     document.write ("41-Niobium"+ "<br>");
+     document.write ("42-Molybdenum"+ "<br>");
+     document.write ("43-Technetium"+ "<br>");
+     document.write ("44-Ruthenium"+ "<br>");
+     document.write ("45-Rhodium"+ "<br>");
+     document.write ("46-Palladium"+ "<br>");
+     document.write ("47-Silver"+ "<br>");
+     document.write ("48-Cadmium"+ "<br>");
+     document.write ("49-Indium"+ "<br>");
+     document.write ("50-Tin"+ "<br>");
+     document.write ("51-Antimony"+ "<br>");
+     document.write ("52-Tellurium"+ "<br>");
+     document.write ("53-Iodine"+ "<br>");
+     document.write ("54-Xenon"+ "<br>");
+     document.write ("55-Caesium"+ "<br>");
+     document.write ("56-Barium"+ "<br>");
+     document.write ("57-Lathanum"+ "<br>");
+     document.write ("58-Cerium"+ "<br>");
+     document.write ("59-Praseodymium"+ "<br>");
+     document.write ("60-Neodymium"+ "<br>");
+     document.write ( "61-Promethium" + "<br>");
+     document.write ("62-Samarium"+ "<br>");
+     document.write("63-Europium"+ "<br>");
+     document.write ("64-Gadalinium"+ "<br>");
+     document.write ("65-Terbium"+ "<br>");
+     document.write ("66-Dysprasium"+ "<br>");
+     document.write ("67-Halmium"+ "<br>");
+     document.write ("68-Erbium "+ "<br>");
+     document.write ("69-Thulium "+ "<br>");
+     document.write ("70-Ytterbium"+ "<br>");
+    
   
+}
